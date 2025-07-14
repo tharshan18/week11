daf2bd09ad72a248ffb4702bdc83907764a6e968
Tharshan Kumar R G <61736284+tharshan18@users.noreply.github.com>
2025-07-14 18:42:45 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ resource "aws_security_group" "web_sg" {
   from_port   = 0
   to_port     = 0
   protocol    = "-1"
-  cidr_blocks = ["0.0.0.0/0"]
+  cidr_blocks = ["35.182.0.0/16"]
   description = "Allow all outbound traffic"
   }
 
