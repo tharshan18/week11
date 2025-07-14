4b1117bc73209efb5b7ee8bebd7d43e807cd40f4
Tharshan Kumar R G <61736284+tharshan18@users.noreply.github.com>
2025-07-14 18:40:40 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,18 +6,20 @@ resource "aws_security_group" "web_sg" {
   name        = "web_sg"
   description = "Allow HTTP from VPC only"
 
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["10.0.0.0/16"]
+  iingress {
+  from_port   = 80
+  to_port     = 80
+  protocol    = "tcp"
+  cidr_blocks = ["10.0.0.0/16"]
+  description = "Allow HTTP traffic from internal network"
   }
 
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+  from_port   = 0
+  to_port     = 0
+  protocol    = "-1"
+  cidr_blocks = ["0.0.0.0/0"]
+  description = "Allow all outbound traffic"
   }
 
   tags = {
